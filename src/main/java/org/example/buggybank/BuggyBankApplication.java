e5216280d87e1b52bc658ea68573c3c20767c793
Anton <biblejij@gmail.com>
2024-09-18 21:27:43 +0300

    first commit

---
+++ src/main/java/org/example/buggybank/BuggyBankApplication.java
@@ -0,0 +1,13 @@
+package org.example.buggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuggyBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuggyBankApplication.class, args);
+    }
+
+}
